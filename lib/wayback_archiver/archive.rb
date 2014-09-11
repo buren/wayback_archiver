42a0f9519f0770cfb41542f932a89000ffb33a7c
Jacob Burenstam <burenstam@gmail.com>
2014-09-11 14:19:22 +0200

    Archive now with parallel threads. Bumped to v0.0.4

--- lib/wayback_archiver/archive.rb
+++ lib/wayback_archiver/archive.rb
@@ -1,18 +1,29 @@
 module WaybackArchiver
   class Archive
+    MAX_THREAD_COUNT = 8
 
-    def self.post(urls)
-      urls.each_with_index do |url, index|
-        request_url = "#{BASE_URL}#{url}"
-        puts "Archiving (#{index + 1}/#{urls.length}): #{url}"
-        begin
-          res = Request.get_response(request_url)
-          puts "#{res.code} => #{res.message}"
-        rescue Exception => e
-          puts "Error message: #{e.message}"
-          puts "Failed to archive: #{url}"
+    def self.post(all_urls)
+      puts "Request will be sent with max #{MAX_THREAD_COUNT} parallel threads"
+  
+      puts "Total urls to be sent: #{all_urls.length}"
+      threads    = Array.new
+      group_size = (all_urls.length / MAX_THREAD_COUNT) + 1
+
+      all_urls.each_slice(group_size).to_a.each do |urls|
+        threads << Thread.new do
+          urls.each_with_index do |url, index|
+            request_url = "#{BASE_URL}#{url}"
+            begin
+              res = Request.get_response(request_url)
+              print "#{url}    #{res.code} => #{res.message} \n"
+            rescue Exception => e
+              puts "Error message: #{e.message}"
+              puts "Failed to archive: #{url}"
+            end
+          end
         end
       end
+      threads.each(&:join)
     end
 
   end
--- lib/wayback_archiver/request.rb
+++ lib/wayback_archiver/request.rb
@@ -7,7 +7,7 @@ module WaybackArchiver
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true if url.include?('https://')
 
-      request   = Net::HTTP::Get.new(uri.request_uri)
+      request  = Net::HTTP::Get.new(uri.request_uri)
       response = http.request(request)
       response
     end
