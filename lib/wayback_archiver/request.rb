b1b81993a40e7fd150c3c14b982a5f945a1384a6
Jacob Burenstam <burenstam@gmail.com>
2014-09-11 09:58:43 +0200

    Fixed: posting https urls, bumped version to 0.0.3

--- lib/wayback_archiver/request.rb
+++ lib/wayback_archiver/request.rb
@@ -2,7 +2,14 @@ module WaybackArchiver
   class Request
 
     def self.get_response(url)
-      Net::HTTP.get_response(URI.parse(url))
+      uri = URI.parse(url)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true if url.include?('https://')
+
+      request   = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+      response
     end
 
   end
