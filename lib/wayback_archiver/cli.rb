93a856fbd2a9a435ce81288ac2792bb1cab25579
Jacob Burenstam Linder <burenstam@gmail.com>
2026-04-05 22:57:15 +0100

    Remove indentation from result lines in CLI output

--- lib/wayback_archiver/cli.rb
+++ lib/wayback_archiver/cli.rb
@@ -62,7 +62,7 @@ module WaybackArchiver
     def format_result_line(n, result)
       label = result.status_label.ljust(6)
       detail = result.status_detail
-      line = "  [#{n}]  #{label}  #{result.uri}"
+      line = "[#{n}]  #{label}  #{result.uri}"
       line << "  #{detail}" if detail
       line
     end
